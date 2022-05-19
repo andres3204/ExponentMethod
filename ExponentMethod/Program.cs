2aba140834324d706b03bcca81c19b88c226bdb6
Andres Enrique Jiménez Mogollón <andresejimenez@hotmail.com>
2022-05-19 17:03:55 -0500

    Add project files.

---
+++ ExponentMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿static int GetPower(int baseNum, int powerNum)
+{
+    int result = 1;
+
+    for (int i = 0; i < powerNum; i++)
+    {
+        result = result * baseNum;
+    }
+
+    return result;
+}
+
+Console.WriteLine(GetPower(3, 3));
